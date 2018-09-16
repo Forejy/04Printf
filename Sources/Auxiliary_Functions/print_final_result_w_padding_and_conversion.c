d30e2a9c7d68ab01876e0cdec91546b7146af174
Forejy <joffrey.marques@gmail.com>
2018-09-16 19:54:54 +0200

    Re-gestion du champs/precision avec conv x et flag # et sa valeur retour

--- Sources/Auxiliary_Functions/print_final_result_w_padding_and_conversion.c
+++ Sources/Auxiliary_Functions/print_final_result_w_padding_and_conversion.c
@@ -151,8 +151,8 @@ size_t		print_final_result(t_flag flag, const char *stock, int len_argument, int
 		total_len += (size_t)len_padding + (size_t)len_precision;
 	else
 		total_len += (size_t)len_padding + (size_t)len_argument;
-	if (((flag.pointer == 2 || flag.hexa == 2) && flag.precision >= 0) && 
-		len_argument >= 2 && len_argument - 2 < flag.precision)
+	if (((flag.pointer == 2 || (flag.hexa == 2 && flag.hash) && flag.precision >= 0) && 
+		len_argument >= 2 && len_argument - 2 < flag.precision))
 		total_len += 2;
 	if (flag.hash && flag.zero && flag.champs > 0 && !flag.less && flag.hexa)
 		// && len_argument - 2 < flag.precision
