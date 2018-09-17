e3f7d51d16909e713414fdfd72a1a2c161bff6f3
Forejy <joffrey.marques@gmail.com>
2018-09-17 20:44:39 +0200

    Gestion de plusieurs format a la suite : %o %ho %hho %15d %d

--- Sources/Auxiliary_Functions/numbers_functions_octal.c
+++ Sources/Auxiliary_Functions/numbers_functions_octal.c
@@ -7,25 +7,20 @@ size_t			my_put_octal(uintmax_t nbr, t_flag flag)
 	char		stock_number[23];
 	int			i;
 	size_t			total_len;
-	uintmax_t		nb;
 
 
 	i = 22;
-	if (nbr < 0)
-		nb = -nbr;
-	else
-		nb = nbr;
-	if (nb == 0 && flag.precision == -1)
+	if (nbr == 0 && flag.precision == -1)
 	{
 		stock_number[i] = '0';
 		i--;
 	}
 	else
 	{
-		while (nb > 0)
+		while (nbr > 0)
 		{
-			stock_number[i] = (char) ((nb % 8) + '0');
-			nb = nb / 8;
+			stock_number[i] = (char) ((nbr % 8) + '0');
+			nbr = nbr / 8;
 			i--;
 		}
 		if (flag.hash == 2)
@@ -33,11 +28,6 @@ size_t			my_put_octal(uintmax_t nbr, t_flag flag)
 			stock_number[i] = '0';
 			i--;
 		}
-	}
-	if (nbr < 0)
-	{
-		stock_number[i] = '-';
-		i--;
 	}
 	total_len = print_final_result(flag, &stock_number[i + 1], 22 - i, 22 - i);
 	return (total_len);
