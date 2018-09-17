e4c94e2d09a9753896f7d60477eb3d37632ce117
Forejy <joffrey.marques@gmail.com>
2018-09-17 22:55:06 +0200

    En cours sur %S : ÊM-M-^QÊM-^XØ‰∏M-

--- Sources/main.c
+++ Sources/main.c
@@ -1622,10 +1622,10 @@ ft_printf(" +d : % +d \n", 0);
 
 	ft_printf("CAS 500\n");
 	ft_printf("ft_printf : %%30S : ");
-	k = ft_printf("{%30S}", L"ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B");
+	k = ft_printf("{%30s}", "ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B");
 	printf("\nret : %d\n", k);
 	ft_printf("   printf : %%30S : ");
-	k = printf("{%30S}", L"ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B");
+	k = printf("{%30s}", "ÊM-M-^QÊM-^XØ‰∏M-ÂM-^O™ÁM-^L´„M-M-^B");
 	printf("\nret : %d\n", k);
 	return (0);
 
