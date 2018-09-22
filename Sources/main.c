457a582e7b0ecf1e8c92152c91e1f76e44703588
Forejy <joffrey.marques@gmail.com>
2018-09-22 15:33:08 +0200

    Modif pour calculer le bon len_padding lorsqu'on a # avec un octal (comme pour un hexa)

--- Sources/main.c
+++ Sources/main.c
@@ -2298,6 +2298,20 @@ ft_printf(" +d : % +d \n", 0);
 	ft_printf("   printf : %% -3zi, 0 : ");
 	k = printf("{%#2o}", 1);
 	printf("\nret : %d\n", k);
+
+	ft_printf("ft_printf : %% -3zi, 0 : ");
+	k = ft_printf("{%52.2i}", 85257866);
+	printf("\nret : %d\n", k);
+	ft_printf("   printf : %% -3zi, 0 : ");
+	k = printf("{%52.2i}", 85257866);
+	printf("\nret : %d\n", k);
+
+	ft_printf("ft_printf : %% -3zi, 0 : ");
+	k = ft_printf("{%52.2i}", 0);
+	printf("\nret : %d\n", k);
+	ft_printf("   printf : %% -3zi, 0 : ");
+	k = printf("{%52.2i}", 0);
+	printf("\nret : %d\n", k);
 	return (0);
 }
 
