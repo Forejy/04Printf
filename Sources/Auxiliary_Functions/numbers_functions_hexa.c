089003e2e545829032a3600b534b1af348995e2d
Forejy <joffrey.marques@gmail.com>
2018-09-24 18:25:37 +0200

    Oubli de mettre le flag.hexa a 2 avec X

--- Sources/Auxiliary_Functions/numbers_functions_hexa.c
+++ Sources/Auxiliary_Functions/numbers_functions_hexa.c
@@ -46,7 +46,6 @@ size_t			my_putnbr_HEXA(uintmax_t nb, t_flag flag)
 	base_hexa = "0123456789ABCDEF";
 	i = 19;
 	total_len = 0;
-	flag.hexa = 2;
 	if (nb == 0 && flag.precision != 0)
 	{
 		stock_number[i] = '0';
@@ -54,6 +53,7 @@ size_t			my_putnbr_HEXA(uintmax_t nb, t_flag flag)
 	}
 	else
 	{
+		flag.hexa = 2;
 		while (nb > 0)
 		{
 			stock_number[i] = base_hexa[(nb % 16)];
