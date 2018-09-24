abca6fec1eba0fafcfa02b34b70606ab408e26f4
Forejy <joffrey.marques@gmail.com>
2018-09-24 18:52:09 +0200

    Affichage de null + champs dans %s

--- Sources/main.c
+++ Sources/main.c
@@ -2500,6 +2500,13 @@ ft_printf(" +d : % +d \n", 0);
 	ft_printf("   printf : %% -3zi, 0 : ");
 	k = printf("{%34.51s}", NULL);
 	printf("\nret : %d\n", k);
+
+	ft_printf("ft_printf : %% -3zi, 0 : ");
+	k = ft_printf("{%-60ls}", L"(null)");
+	printf("\nret : %d\n", k);
+	ft_printf("   printf : %% -3zi, 0 : ");
+	k = printf("{%-60ls}",  L"(null)");
+	printf("\nret : %d\n", k);
 	return (0);
 }
 
